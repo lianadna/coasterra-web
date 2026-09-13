644986f9504a4b8e2dd6730669b4720982f195a1
Yulian Adiprana <192160137+lianadna@users.noreply.github.com>
2026-09-13 19:47:20 +0700

    hapus route dan controller index2 - index5

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,26 +37,6 @@ class HomeController extends Controller
         ]);
     }
 
-    public function index2()
-    {
-        return view('home.index2');
-    }
-
-    public function index3()
-    {
-        return view('home.index3');
-    }
-
-    public function index4()
-    {
-        return view('home.index4');
-    }
-
-    public function index5()
-    {
-        return view('home.index5');
-    }
-
     public function about()
     {
         return view('about', [
